f646d4f74d0a999797672e3051ec809e1c571ed7
zachnitzkin <znitz23@gmail.com>
2023-04-20 10:39:34 -0500

    part one complete

---
+++ db/index.js
@@ -0,0 +1,35 @@
+const { Client } = require('pg');
+
+const client = new Client('postgres://localhost:5432/juicebox_dev');
+
+async function createUser({ username, password }) {
+    try {
+      const { rows } = await client.query(`
+        INSERT INTO users (username, password)
+        VALUES ($1, $2)
+        ON CONFLICT (username) DO NOTHING
+        Returning *;
+        `, [username, password]
+      );
+  
+      return rows
+    } catch (error) {
+      throw error;
+    }
+  }
+
+async function getAllUsers() {
+    const { rows } = await client.query(
+      `SELECT id, username 
+      FROM users;
+    `);
+  
+    return rows;
+  }
+  
+
+module.exports = {
+    client,
+    getAllUsers,
+    createUser
+  }
